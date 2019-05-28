2dee1712b74c13c3777d4c04b39f6ffac3b7f39c
Prakash <rprakashsvn@gmail.com>
2019-05-28 14:22:36 +0530

    modified test2 java class

--- src/JavaBasics/Test2.java
+++ src/JavaBasics/Test2.java
@@ -5,6 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 
 		System.out.println("Create pull request from Master to Prakash Branch");
+		System.out.println("git difference");
 
 	}
 
